c2e4e5d8490bb5aeb9924af0664cf021310a7a8b
Raphael Fagundes <fagundesraphael7@gmail.com>
2025-09-13 02:11:22 -0400

    fix remove template

--- lua/config/autocmds/init.lua
+++ lua/config/autocmds/init.lua
@@ -5,4 +5,3 @@ vim.api.nvim_exec = vim.api.nvim_exec2
 require("config.autocmds.transparency")
 require("config.autocmds.gitsigns")
 require("config.autocmds.lazyvim")
-require("config.autocmds.templates")
