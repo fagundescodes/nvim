d4c3da5940acdac699e09df16071b1ae6ca787d7
Raphael Fagundes <fagundesraphael7@gmail.com>
2026-01-15 21:49:57 -0400

    fix treesitter configs

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -29,4 +29,5 @@ end
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
+vim.opt_local.autoindent = true
 vim.opt_local.smartindent = true
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,27 +1,12 @@
 local ts = require("nvim-treesitter")
 
-local waiting_buffers = {}
-local installing_langs = {}
-
 local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
-
-local function enable_treesitter(buf, lang)
-  if not vim.api.nvim_buf_is_valid(buf) then
-    return false
-  end
-
-  local ok = pcall(vim.treesitter.start, buf, lang)
-  if ok then
-    vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end
-  return ok
-end
 
 vim.api.nvim_create_autocmd("User", {
   group = group,
   pattern = "LazyDone",
   once = true,
-  desc = "Install core treesitter parsers",
+  desc = "Install treesitter parsers",
   callback = function()
     ts.install({
       "vim",
@@ -50,9 +35,7 @@ vim.api.nvim_create_autocmd("User", {
       "sql",
       "markdown",
       "markdown_inline",
-    }, {
-      max_jobs = 8,
-    })
+    }, { max_jobs = 8 })
   end,
 })
 
@@ -66,55 +49,12 @@ local ignore_filetypes = {
 
 vim.api.nvim_create_autocmd("FileType", {
   group = group,
-  desc = "Enable treesitter highlighting and indentation",
+  desc = "Enable treesitter highlighting",
   callback = function(event)
     if ignore_filetypes[event.match] then
       return
     end
 
-    local lang = vim.treesitter.language.get_lang(event.match) or event.match
-    local buf = event.buf
-
-    if not enable_treesitter(buf, lang) then
-      waiting_buffers[lang] = waiting_buffers[lang] or {}
-      waiting_buffers[lang][buf] = true
-
-      if not installing_langs[lang] then
-        installing_langs[lang] = true
-        local task = ts.install({ lang })
-
-        if task and task.await then
-          task:await(function()
-            vim.schedule(function()
-              installing_langs[lang] = nil
-
-              local buffers = waiting_buffers[lang]
-              if buffers then
-                for b in pairs(buffers) do
-                  enable_treesitter(b, lang)
-                end
-                waiting_buffers[lang] = nil
-              end
-            end)
-          end)
-        else
-          installing_langs[lang] = nil
-          waiting_buffers[lang] = nil
-        end
-      end
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufDelete", {
-  group = group,
-  desc = "Clean up treesitter waiting buffers",
-  callback = function(event)
-    for lang, buffers in pairs(waiting_buffers) do
-      buffers[event.buf] = nil
-      if next(buffers) == nil then
-        waiting_buffers[lang] = nil
-      end
-    end
+    pcall(vim.treesitter.start, event.buf)
   end,
 })
